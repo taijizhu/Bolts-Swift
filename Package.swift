e06f074721c4c61b236ca16fe38804ad5cd2244b
Nikita Lutsenko <nlutsenko@me.com>
2019-09-27 18:40:27 -0700

    Add SwiftPM support.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "BoltsSwift",
+    platforms: [.macOS(.v10_10),
+                .iOS(.v8),
+                .tvOS(.v9),
+                .watchOS(.v3)],
+    products: [
+        .library(
+            name: "BoltsSwift",
+            targets: ["BoltsSwift"]),
+    ],
+    targets: [
+        .target(
+            name: "BoltsSwift",
+            path: "Sources/BoltsSwift"),
+        .testTarget(
+            name: "BoltsSwiftTests",
+            dependencies: ["BoltsSwift"],
+            path: "Tests"),
+    ],
+    swiftLanguageVersions: [.v4,
+                            .v4_2,
+                            .v5]
+)
